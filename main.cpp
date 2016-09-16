2f0e94291d1f43b40c63337a664d6ac46379d039
Pete Donegan <pdonegan@stumail.jccc.edu>
2016-09-15 19:36:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    bool done = false;
+	bool done = false;
 	const int foodConsumed_PerPersonPerDay = 2; 
 	const int oxygenCreated_PerDay = 5;
 	const int wasteCreated_PerPersonPerDay = 3;
@@ -22,7 +25,8 @@ int main()
 
 	while (done == false)
 	{
-		cout << "Mars Colony main menu" << endl
+		cout<< "---------------------------" << endl
+			<< "Mars Colony main menu" << endl
 			<< "1. Set Mission Parameters" << endl
 			<< "2. Run Simulation" << endl
 			<< "3. Quit" << endl
@@ -33,7 +37,27 @@ int main()
 
 		if (choice == 1)
 		{
-			cout << "Mission Parameters" << endl;
+			cout << "A. Food grown per day" << endl;
+				cin >> foodGrown_PerDay;
+				cout << "B. Cleanup per person per day" << endl;
+				cin >> cleanup_PerPersonPerDay;
+				cout << "C. Starting units of food" << endl;
+				cin >> foodCount;
+				cout << "D. Mission length, in days" << endl;
+				cin >> missionLength;
+				cout << "E. People on the mission" << endl;
+				cin >> peopleCount;
+				cout << "F. Team name" << endl;
+				cin >> teamName;
+				cout << endl;
+
+				cout << "foodGrown_PerDay: " << foodGrown_PerDay << endl;
+				cout << "cleanup_PerPersonPerDay: " << cleanup_PerPersonPerDay << endl;
+				cout << "foodCount: " << foodCount << endl;
+				cout << "missionLength: " << missionLength << endl;
+				cout << "peopleCount: " << peopleCount << endl;
+				cout << "teamName: " << teamName << endl;
+				cout << endl;
 		}
 		if (choice == 2)
 		{
@@ -44,6 +68,6 @@ int main()
 			done = true;
 		}
 	}
-	
+
 	return 0;
 }
